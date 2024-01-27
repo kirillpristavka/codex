7a4045df938c20a2786f59b93e967e2019fd1a90
kirillpristavka1 <157869685+kirillpristavka1@users.noreply.github.com>
2024-01-27 08:35:33 +0300

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Привет от Кодекса',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        // const response = await openai.createCompletion({
+        //     model: "text-davinci-003",
+        //     prompt: `${prompt}`,
+        //     temperature: 0,
+        //     max_tokens: 3000,
+        //     top_p: 1,
+        //     frequency_penalty: 0.5,
+        //     presence_penalty: 0,
+        // });
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: prompt }],
+        });
+
+        res.status(200).send({
+            bot: response.choices[0].message.content
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+});
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
